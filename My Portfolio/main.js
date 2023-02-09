61ed8498a05c3e97e9cdab96b7badecfaf5e0d03
ChristopherEveningStar13 <124846856+ChristopherEveningStar13@users.noreply.github.com>
2023-02-08 20:59:51 -0400

    Add files via upload
    First Commit

---
+++ My Portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text" , {
+  strings: [ " Cyber Security Specialist", " Air Gap Speacialist", " Crypto-Analyst", " Lucky Man!!!"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
